65225a4c50e69a3f454f67fcf9926c1f4b241240
Andrew Nissen <andrew_nissen@yahoo.com>
2013-10-08 20:07:06 -0400

    first commit

---
+++ web.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+app.use(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+var request = require('request');
+
+var urls = [];
+urls.push("http://wakeup-bot.herokuapp.com/");
+urls.push("http://tinkerbot.herokuapp.com/");
+
+setInterval(function(){
+
+  for (var i = urls.length - 1; i >= 0; i--) {
+    request(urls[i], function (error, response, body){
+      console.log(urls[i] + response);
+    });
+  }
+
+}, 30 * 60 * 1000);
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
